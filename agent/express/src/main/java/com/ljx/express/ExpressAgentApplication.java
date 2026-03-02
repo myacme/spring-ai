70b8fc9aadcef4460eb43174ee11fa1e86d236d0
liujx <1030375081@qq.com>
2026-03-02 17:24:13 +0800

    refactor(express): 移除冗余组件扫描配置并更新天气代理API密钥
    - 移除了ExpressAgentApplication中的@ComponentScan注解，因为@SpringBootApplication已包含此功能
    - 在weather agent的application.yml中设置了默认的ZHIPUAI_API_KEY值
    - 更新了API密钥的占位符语法以包含默认值

--- agent/express/src/main/java/com/ljx/express/ExpressAgentApplication.java
+++ agent/express/src/main/java/com/ljx/express/ExpressAgentApplication.java
@@ -2,10 +2,8 @@ package com.ljx.express;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.ljx.express")
 public class ExpressAgentApplication {
     
     public static void main(String[] args) {
